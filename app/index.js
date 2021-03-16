60b47b55e4463f3525ef361962aaad1c5437b03d
Mark Butcher <markjamesbutcher@gmail.com>
2021-03-16 23:15:10 +1100

    ready for P2P server

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const Blockchain = require('../blockchain');
 
 
@@ -7,10 +9,17 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/blocks', (req, res) => {
   res.json(bc.chain);
 });
 
+app.post('/mine', (req, res) => {
+  const block = bc.addBlock(req.body.data)
+  // console.log(`New block added: ${block.toString()}`);
+  res.redirect('/blocks')
+});
 
 
 app.listen(HTTP_PORT, () => console.log(`Listing on port ${HTTP_PORT}`))
