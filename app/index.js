78ee364c7d388e44ec7eadd362a3b14f576000b0
Mark Butcher <markjamesbutcher@gmail.com>
2021-03-16 23:00:15 +1100

    . removed from get reguest endpoint

--- app/index.js
+++ app/index.js
@@ -1,18 +1,16 @@
 const express = require('express');
-const Blockchain = require('../blockchain/index');
+const Blockchain = require('../blockchain');
 
 
-const HTTP_PORT = process.env.HTTP_PORT || 3008;
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
 
-app.get('./blocks', (req, res) => {
+app.get('/blocks', (req, res) => {
   res.json(bc.chain);
-})
+});
 
-app.post('./blocks', (req, res) => {
-  req.json(bc.chain);
-})
+
 
 app.listen(HTTP_PORT, () => console.log(`Listing on port ${HTTP_PORT}`))
