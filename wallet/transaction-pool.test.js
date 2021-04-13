b3c750cd97be521dc81aabdbb0e86c9b7825a5ac
Mark Butcher <markjamesbutcher@gmail.com>
2021-04-13 18:08:50 +1000

    transaction pool, updateOrAddTransaction and add transaction function to wallet

---
+++ wallet/transaction-pool.test.js
@@ -0,0 +1,26 @@
+const TransactionPoll = require('./transaction-pool');
+const Transaction = require('./transaction');
+const Wallet = require('./index');
+
+describe('TransactionPool', () => {
+  let tp, transaction, wallet;
+
+  beforeEach(() => {
+    tp = new TransactionPoll();
+    wallet = new Wallet();
+    transaction = Transaction.newTransaction(wallet, 'r4n-4ddr355', 30)
+    tp.updateOrAddTransaction(transaction);
+  });
+
+  it('adds a transaction to the pool', () => {
+    expect(tp.transactions.find(t => t.id === transaction.id)).toEqual(transaction);
+  });
+  it('updates a transaction tin the pool', () => {
+    let oldTransaction = JSON.stringify(transaction);
+    const newTransaction = transaction.update(wallet, 'foo-4dd355', 40);
+    tp.updateOrAddTransaction(newTransaction)
+
+    expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id))).not.toEqual(oldTransaction)
+  });
+
+});
